6deea82c056cf2e291d89f7adf5f6a049409c137
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-12 11:16:17 +0800

    await

--- Canchu/api1.0/models/mysql.js
+++ Canchu/api1.0/models/mysql.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql2/promise'); 
 
-async function Connect(){
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'user',
-        password: process.env.PASSWORD,
-        database: 'canchu'
-    });
-    return connection;
-}
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: process.env.PASSWORD,
+    database: 'canchu'
+});
 
 module.exports = {
-    Connect
+    connection
 };
