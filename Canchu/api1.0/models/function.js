276b6c069fbb0fb5e26649f4430f0cbabfed7a88
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-12 16:51:07 +0800

    1234

---
+++ Canchu/api1.0/models/function.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+function verifyAccesstoken(req, res, next){
+    let token = req.headers.authorization;
+    if(!token){
+        res.status(401).json({error : 'No token'});
+        return;
+    }
+    token = token.substring(7, token.length);
+    try {
+        var decoded = jwt.verify(token, process.env.SECRETKEY);
+        req.decoded = decoded;
+    } 
+    catch(err) {
+        res.status(403).json({error : 'Wrong token'});
+        console.log(err);
+        return;
+    }
+    next();
+}
+
+module.exports = {
+    verifyAccesstoken
+};
