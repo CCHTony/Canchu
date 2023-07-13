7ba8fca7e3e6ec1b2be20cc6f08bd1a18668dc7a
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-13 16:47:50 +0800

    123456

--- Canchu/api1.0/router/posts.js
+++ Canchu/api1.0/router/posts.js
@@ -10,7 +10,7 @@ router.post('/', verifyAccesstoken, async(req, res) => {
     const my_id = req.decoded.id;
     const context = req.body.context;
 
-    let mysQuery = 'INSERT INTO posts`(`poster_id`, `created_at`, `context`, `like_count`, `comment_count`) VALUES(?,NOW(),?,?,?)';
+    let mysQuery = 'INSERT INTO posts (`poster_id`, `created_at`, `context`, `like_count`, `comment_count`) VALUES (?, NOW(), ?, ?, ?)';
     const [post] = await connection.execute(mysQuery, [my_id, context, 0, 0]);
     const results = {
         "data": {
