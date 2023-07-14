9f8b58c6298bdee25517e2cf7b1fb423d0aaa8dd
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-14 09:42:13 +0800

    123456

--- Canchu/api1.0/router/posts.js
+++ Canchu/api1.0/router/posts.js
@@ -45,4 +45,43 @@ router.put('/:id', verifyAccesstoken, async(req, res) => {
 });
 
 
+router.post('/:id/like', verifyAccesstoken, async(req, res) => {
+    const connection = await connectionPromise;
+    const post_id = req.params.id;
+    const my_id = req.decoded.id;
+
+    let mysQuery = 'INSERT INTO `likes` (`post_id`, `user_id`, `created_at`) VALUES (?, ?, NOW())';
+    const [like] = await connection.execute(mysQuery, [post_id, my_id]);
+    console.log(like);
+    const results = {
+        "data": {
+            "post": {
+                "id": post_id
+            }
+        }
+    }
+    res.json(results);
+});
+
+
+router.delete('/:id/like', verifyAccesstoken, async(req, res) => {
+    const connection = await connectionPromise;
+    const post_id = req.params.id;
+    const my_id = req.decoded.id;
+
+    let mysQuery = 'DELETE FROM `likes` WHERE `post_id` = ? AND `user_id` = ?';
+    const [like] = await connection.execute(mysQuery, [post_id, my_id]);
+    console.log(like);
+    const results = {
+        "data": {
+            "post": {
+                "id": post_id
+            }
+        }
+    }
+    res.json(results);
+});
+
+
+
 module.exports = router;
