794c1fbf8a5745bb4cb68bd57188555ed37a34c8
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-20 11:37:32 +0800

    add gitignore

--- Canchu/test.js
+++ Canchu/test.js
@@ -1,11 +1,11 @@
-import { createClient } from 'redis';
+const redis = require('redis');
 
 const redisOptions = {
   host: 'localhost', // Redis服务器的主机地址
   port: 6379, // Redis服务器的端口号
 };
 
-const client = createClient(redisOptions);
+const client = redis.createClient(redisOptions);
 
 client.on('error', err => console.log('Redis Client Error', err));
 
