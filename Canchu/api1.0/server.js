7edb87e767c5a2b49721bf046466ae9f8ad650e4
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-24 15:33:28 +0800

    123456789

--- Canchu/api1.0/server.js
+++ Canchu/api1.0/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
 require('dotenv').config();
 
+// 設定每分鐘允許的最大請求數量為 100
+const limiter = rateLimit({
+  windowMs:  1000, // 一分鐘
+  max: 10, // 最大請求數量
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+// 將 Rate Limiting 中間件應用到所有路由上
+app.use(limiter);
 
 const usersRouter = require('./router/users');
 const friendsRouter = require('./router/friends');
