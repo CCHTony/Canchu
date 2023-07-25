d079eaf7ddb41b1405bae662c9ea0a224b35f22f
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-25 18:00:42 +0800

    123456

--- Canchu/api1.0/tests/signup.test.js
+++ Canchu/api1.0/tests/signup.test.js
@@ -2,13 +2,13 @@ const request = require('supertest');
 const {app} = require('../server'); // 引入你的 Express 應用程式
 
 
-// 測試使用者註冊 API
-describe('Signup API', () => {
+// 成功註冊
+describe('Signup', () => {
   it('should register a new user and return user data', async () => {
     // 模擬請求的使用者資料
     const userData = {
       name: 'John Doe',
-      email: 'joh1n.doe@example.com',
+      email: 'john123456.doe@example.com',
       password: 'password123',
     };
 
@@ -29,12 +29,52 @@ describe('Signup API', () => {
   });
 });
 
-// 測試使用者註冊 API
-describe('Signup API', () => {
+// 資料缺失
+describe('missing field', () => {
   it('should register a new user and return user data', async () => {
     // 模擬請求的使用者資料
     const userData = {
       email: 'joh1n.doe@example.com',
+      password: 'password123',
+    };
+
+    // 在這裡使用 supertest 來發送 POST 請求，模擬使用者註冊
+    const res = await request(app)
+      .post('/api/1.0/users/signup')
+      .send(userData);
+
+    // 確認伺服器回應的狀態碼是否正確
+    expect(res.status).toBe(400);
+  });
+});
+
+// 重複註冊
+describe('Signup again', () => {
+  it('should register a new user and return user data', async () => {
+    // 模擬請求的使用者資料
+    const userData = {
+      name: 'John Doe',
+      email: 'john123456.doe@example.com',
+      password: 'password123',
+    };
+
+    // 在這裡使用 supertest 來發送 POST 請求，模擬使用者註冊
+    const res = await request(app)
+      .post('/api/1.0/users/signup')
+      .send(userData);
+
+    // 確認伺服器回應的狀態碼是否正確
+    expect(res.status).toBe(403);
+  });
+});
+
+// 信箱錯誤
+describe('wrong email', () => {
+  it('should register a new user and return user data', async () => {
+    // 模擬請求的使用者資料
+    const userData = {
+      name: 'John Doe',
+      email: 'john123456.doeexample.com',
       password: 'password123',
     };
 
