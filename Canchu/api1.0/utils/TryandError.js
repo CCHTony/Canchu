560cfa85d86d054169e6c069fb726c5d4368a502
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-25 14:12:16 +0800

    123456789

--- Canchu/api1.0/utils/TryandError.js
+++ Canchu/api1.0/utils/TryandError.js
@@ -1,14 +1,13 @@
-async function TryErr(asyFunc){
+async function TryErr(asyFunc, res){
   try{
     await asyFunc
   }
   catch (err) {
 		// 若有任何錯誤，回傳伺服器錯誤並顯示錯誤訊息在後端
-		res.status(500).json({ error: "Server Error." });
-		console.log(err);
+    console.log(err);
+		return res.status(500).json({ error: "Server Error." });
 	}
 }
-
 
 module.exports = {
   TryErr
