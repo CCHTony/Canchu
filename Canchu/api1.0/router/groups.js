d6be45bbb077f41c588ae558d0e07cbfa73ebd06
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-07-28 12:40:30 +0800

    123456

--- Canchu/api1.0/router/groups.js
+++ Canchu/api1.0/router/groups.js
@@ -5,8 +5,10 @@ const router = express.Router(); // 建立 Express 路由器
 const {
   createGroup,
   deleteGroup,
-  joinGroup
+  joinGroup,
+  getPendingMembers
 } = require('../Controller/groupsController')
+
 // 引入驗證 Access Token 的函式
 const verifyAccesstoken = require('../utils/function').verifyAccesstoken;
 // 引入自訂的 TryErr 函式，用於處理錯誤並統一回傳格式
@@ -16,7 +18,7 @@ const TryErr = require('../utils/TryandError').TryErr;
 router.post('/', verifyAccesstoken, (req, res) => TryErr(createGroup(req, res), res));
 router.delete('/:group_id', verifyAccesstoken, (req, res) => TryErr(deleteGroup(req, res), res));
 router.post('/:group_id/join', verifyAccesstoken, (req, res) => TryErr(joinGroup(req, res), res));
-router.post('/:group_id/join', verifyAccesstoken, (req, res) => TryErr(joinGroup(req, res), res));
+router.get('/:group_id/join', verifyAccesstoken, (req, res) => TryErr(getPendingMembers(req, res), res));
 
 
 // 匯出這些路由設定
