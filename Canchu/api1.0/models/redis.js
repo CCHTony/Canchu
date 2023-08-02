8461d513f9923b0505074c8499e0c34089163473
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-02 14:41:20 +0800

    123456

--- Canchu/api1.0/models/redis.js
+++ Canchu/api1.0/models/redis.js
@@ -4,7 +4,7 @@ const Redis = require("ioredis");
 // Create a Redis instance.
 const redis = new Redis({
   port: 6379, // Redis port
-  host: "localhost", // Redis host
+  host: "redis_container", // Redis host
   db: 0, // Database 0
 });
 
