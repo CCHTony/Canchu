7f8485f315f73a449a0922a3111751b704dbb013
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-07 15:28:45 +0800

    123456

--- Canchu/api1.0/models/mysql.js
+++ Canchu/api1.0/models/mysql.js
@@ -1,18 +1,11 @@
 const mysql = require('mysql2/promise'); 
 
-let database;
-if(process.env.NODE_ENV === 'develop'){
-    database = process.env.DATABASE;
-}
-else if(process.env.NODE_ENV === 'test'){
-    database = process.env.DATABASEFORTEST;
-}
 
 const connectionPromise =  mysql.createPool({
     host: 'mysql',
     user: 'user',
     password: process.env.PASSWORD,
-    database: database
+    database: process.env.DATABASE
 });
 
 
