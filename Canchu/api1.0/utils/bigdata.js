9927c7e103a4878bdddaa1d5b19b298ab9598c69
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-09 14:44:29 +0800

    123456

--- Canchu/api1.0/utils/bigdata.js
+++ Canchu/api1.0/utils/bigdata.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto'); // 引入 crypto 套件，用於加密處理
 // 引入資料庫連線
-const connectionPromise = require('./Canchu/api1.0/models/mysql').connectionPromise;
+const connectionPromise = require('../models/mysql').connectionPromise;
 
 const TIMES = 5000
 const NAME = 'test'
@@ -8,7 +8,7 @@ const EMAIL = 'test@test.com'
 const PASSWORD = 'test'
 
 
-async function generateData(times){
+async function generateData(){
   const connection = await connectionPromise;
   // 使用 crypto 加密密碼
 	const hashedPassword = crypto.createHash('sha256').update(PASSWORD).digest('hex');
@@ -17,12 +17,14 @@ async function generateData(times){
 	const [results] = await connection.execute(signupQuery, [NAME, EMAIL, hashedPassword, null, 'native']);
   const id = results.insertId
 
-  for(let i = 0; i < times; i++){
+  for(let i = 0; i < TIMES; i++){
     const postQuery = 'INSERT INTO posts (poster_id, created_at, context, like_count, comment_count) VALUES (?, NOW(), ?, ?, ?)';
     await connection.execute(postQuery, [id, i , 0, 0]); // 在資料庫中新增帖子
   }
 }
 
-generateData(TIMES);
+module.exports = {
+	generateData
+};
 
 
