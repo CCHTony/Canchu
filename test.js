d5409eec6b38c0a98e9f22fb501382ecf8fa5f30
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-09 15:49:12 +0800

    123456

--- test.js
+++ test.js
@@ -17,7 +17,9 @@ export const options = {
 };
 // test HTTP
 export default function () {
-  const res = http.get('https://52.64.240.159/api/1.0/posts/search');
+  const res = http.get('https://52.64.240.159/api/1.0/posts/search', {headers: {
+    Authorization : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6InRlc3QxMTIzNDUiLCJlbWFpbCI6InRlYWY4ODg3QHRlc3QuY29tIiwicHJvdmlkZXIiOiJuYXRpdmUiLCJwaWN0dXJlIjpudWxsLCJpYXQiOjE2OTE1NjU0NjEsImV4cCI6MTY5MTY1MTg2MX0.2LOa-StHKPzXc7Mb1zXixjGzEpPr9whCpTodEAQsJDA'
+  }});
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
