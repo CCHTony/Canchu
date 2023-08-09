d2ea3d737ff87623f2d9fad9d54ff447fa646060
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-09 15:01:10 +0800

    123456

--- Canchu/api1.0/models/mysql.js
+++ Canchu/api1.0/models/mysql.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const connectionPromise =  mysql.createPool({
     host: 'canchu-database.cknspj9yw05b.ap-southeast-2.rds.amazonaws.com',
     user: 'user',
-    password: process.env.PASSWORD,
+    password: 'password',
     database: process.env.DATABASE
 });
 
