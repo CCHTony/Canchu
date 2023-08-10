28fb0a2df4377539428a13dfc7ff9fa6e4a4deb2
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-10 15:16:01 +0800

    123456

--- Canchu/api1.0/models/mysql.js
+++ Canchu/api1.0/models/mysql.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // 載入 .env 檔案中的環境變數
 
 
 const connectionPromise =  mysql.createPool({
-    host: 'canchu-database.cknspj9yw05b.ap-southeast-2.rds.amazonaws.com',
+    host: 'canchu-database-1.cknspj9yw05b.ap-southeast-2.rds.amazonaws.com',
     user: 'user',
     password: 'password',
     database: process.env.DATABASE
